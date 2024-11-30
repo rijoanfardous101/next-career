0c48c24bee59f0025760bae0f5dda9cd007cb854
Rijoan Fardous <rijoanfardous101@gmail.com>
2024-11-30 09:09:55 +0600

    signup post connection features added

---
+++ backend/src/index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import connectionRoutes from "./routes/connection.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "5mb" }));
+app.use(cookieParser());
+
+// CORS Configuration for Development
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // Allow only frontend URL
+      credentials: true, // Allow credentials (cookies)
+    })
+  );
+}
+
+// Define Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/connections", connectionRoutes);
+
+// Serve Static Files in Production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+// Start the Server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
